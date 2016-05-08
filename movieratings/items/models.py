dd75f6666c57962464794f3a49012488d157f593
Greg Johnston <greg.a.johnston@gmail.com>
2016-05-08 14:53:38 -0400

    Added models to items directory

--- movieratings/items/models.py
+++ movieratings/items/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=150)
+    rel_date = models.DateTimeField('date release')
+    imdb_url = models.CharField(max_length=200)
+    genre = models.CharField(max_length=50)
+
+
+class Rater(models.Model):
+    age = models.IntegerField(default=0)
+    gender = models.CharField(max_length=1)
+    occupation = models.Charfield(max_length=150)
+    zip_code = models.CharField(max_length=10)
+
+
+class Rating(models.Model):
+    rater = models.ForeignKey(Rater, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
