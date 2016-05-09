d7919c57e6fff7508df96b0c8543b69f13ad1e07
Greg Johnston <greg.a.johnston@gmail.com>
2016-05-09 16:19:36 -0400

    Many changes, imported data successfully except for ratings

--- movieratings/importer.py
+++ movieratings/importer.py
@@ -1,33 +1,67 @@
 import csv
 import json
+from datetime import date, datetime
 
 
-MOVIE_COLUMNS = ['movie_id', 'movie_title', 'date_added',
-                 'imdb_url'] + ['genre {}'.format(x) for x in range(19)]
-RATER_COLUMNS = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+MOVIE_COLUMNS = ['id', 'title', 'date_added', 'drop',
+                 'imdb_url'] + ['genre_{}'.format(x) for x in range(1, 20)]
+RATER_COLUMNS = ['id', 'age', 'gender', 'occupation', 'zip_code']
 RATING_COLUMNS = ['user_id', 'movie_id', 'rating', 'timestamp']
 
 MOVIE_FILE = 'data/u.item'
 RATER_FILE = 'data/u.user'
 RATING_FILE = 'data/u.data'
 
-MOVIE_JSON = 'items/movie.json'
-RATER_JSON = 'items/rater.json'
-RATING_JSON = 'items/rating.json'
+MOVIE_MODEL = 'items.movie'
+RATER_MODEL = 'items.rater'
+RATING_MODEL = 'items.rating'
+
+MOVIE_JSON = 'items/fixtures/movie.json'
+RATER_JSON = 'items/fixtures/rater.json'
+RATING_JSON = 'items/fixtures/rating.json'
 
 
-def read_data(input_file, output_file, column_names):
+def read_data(input_file, output_file, column_names, model_name):
     with open(output_file, 'w+') as j:
         with open(input_file, encoding='latin_1') as f:
-            reader = csv.DictReader(f, fieldnames=column_names, delimiter='|')
-            output = json.dumps([row for row in reader])
-        j.write(output)
+            delim = '|'
+            if input_file == 'data/u.data':
+                delim = '\t'
+            reader = csv.DictReader(f, fieldnames=column_names,
+                                    delimiter=delim)
+            output = "["
+            counter = 0
+            for row in reader:
+                if input_file == 'data/u.item':
+                    index_value = row['id']
+
+                    date_added = row['date_added']
+                    if date_added:
+                        date_added = datetime.strptime(date_added, '%d-%b-%Y')
+                    else:
+                        date_added = date(1900, 1, 1)
+
+                    row['date_added'] = date_added.strftime('%Y-%m-%d')
+
+                    del row['drop']
+                    del row['id']
+                if input_file == 'data/u.user':
+                    index_value = row['id']
+                    del row['id']
+                if input_file == 'data/u.data':
+                    counter += 1
+                    index_value = counter
+                output += "{" + "\"model\": \"{}\", \"pk\": {}, ".format(
+                            model_name, index_value)
+                output += "\"fields\": {}".format(json.dumps(row)) + "}, "
+            output += "]"
+            j.write(output)
 
 
 def main():
-    read_data(MOVIE_FILE, MOVIE_JSON, MOVIE_COLUMNS)
-    read_data(RATER_FILE, RATER_JSON, RATER_COLUMNS)
-    read_data(RATING_FILE, RATING_JSON, RATING_COLUMNS)
+    read_data(MOVIE_FILE, MOVIE_JSON, MOVIE_COLUMNS, MOVIE_MODEL)
+    read_data(RATER_FILE, RATER_JSON, RATER_COLUMNS, RATER_MODEL)
+    read_data(RATING_FILE, RATING_JSON, RATING_COLUMNS, RATING_MODEL)
 
 if __name__ == '__main__':
     main()
--- movieratings/items/models.py
+++ movieratings/items/models.py
@@ -4,9 +4,28 @@ from django.db import models
 class Movie(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
     title = models.CharField(max_length=150)
-    add_date = models.DateTimeField('date added')
+    date_added = models.DateField('date added')
     imdb_url = models.CharField(max_length=200)
-    genre = models.CharField(max_length=50)
+    genre_1 = models.CharField(max_length=50, default=0)
+    genre_2 = models.CharField(max_length=50, default=0)
+    genre_3 = models.CharField(max_length=50, default=0)
+    genre_4 = models.CharField(max_length=50, default=0)
+    genre_5 = models.CharField(max_length=50, default=0)
+    genre_6 = models.CharField(max_length=50, default=0)
+    genre_7 = models.CharField(max_length=50, default=0)
+    genre_8 = models.CharField(max_length=50, default=0)
+    genre_9 = models.CharField(max_length=50, default=0)
+    genre_10 = models.CharField(max_length=50, default=0)
+    genre_11 = models.CharField(max_length=50, default=0)
+    genre_12 = models.CharField(max_length=50, default=0)
+    genre_13 = models.CharField(max_length=50, default=0)
+    genre_14 = models.CharField(max_length=50, default=0)
+    genre_15 = models.CharField(max_length=50, default=0)
+    genre_16 = models.CharField(max_length=50, default=0)
+    genre_17 = models.CharField(max_length=50, default=0)
+    genre_18 = models.CharField(max_length=50, default=0)
+    genre_19 = models.CharField(max_length=50, default=0)
+    genre_20 = models.CharField(max_length=50, default=0)
 
     def __str__(self):
         return self.title
@@ -27,6 +46,7 @@ class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, default=0)
     rater = models.ForeignKey(Rater, on_delete=models.CASCADE, default=0)
     rating = models.IntegerField(default=0)
+    timestamp = models.IntegerField(default=0)
 
     def __str__(self):
         return self.rating
